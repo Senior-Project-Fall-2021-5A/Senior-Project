c06662f8fa4f7ea809d4a7dbcff394dca293f547
frostbyte000jm <james@getjames.com>
2021-09-10 00:58:00 -0400

    Got a button to work

--- telemedicine-app/src/App.js
+++ telemedicine-app/src/App.js
@@ -3,24 +3,39 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 
 import Button from 'react-bootstrap/Button'
 import Header from './components/header';
-import { Router, Route, Switch } from "react-router";
-import {Link} from 'react-router-dom'
+import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
+import HomePage from "./pages/homepage";
+import About from "./pages/about";
+// import { Button } from 'bootstrap';
+// import { Router, Route, Switch } from "react-router";
+// import {Link} from 'react-router-dom'
 
 
 
 function App() {
   return (
-    <div className="App">
-    <Button variant="primary">
-      <Link to='./pages/homepage.js'>
-        Hello
-      </Link>
-    </Button>{''}
-      
-    
-    
+    <Router>
+      <div className="App">
+        <Header/>
+        <Link to="/about">
+        <Button variant="primary">
+          About
+        </Button>{' '}
+        </Link>
+        <Switch>
+          <Route path="/about" component={About} /> 
+          <Route path="/" component={HomePage} />
+          
+        </Switch>
+        
 
-    </div>
+      
+        
+      
+      
+
+      </div>
+    </Router>
   );
 }
 
---
+++ telemedicine-app/src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Form, Button} from 'react-bootstrap'
+
+function about() {
+    return (
+        <div>
+            About
+        </div>
+    )
+}
+
+export default about
