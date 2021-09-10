1ec5f4728987b08b7fa199e113f760d513cb8324
Jorge <jorgecab796@gmail.com>
2021-09-10 00:03:23 -0400

    Getting our feet wet
    We love being developers

---
+++ telemedicine-app/src/components/form.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Form(){
+    return 
+}
+
+export default Form;
+
---
+++ telemedicine-app/src/components/header.js
@@ -0,0 +1,8 @@
+import React from 'react'
+
+function Header(){
+    return <header>This and That</header>
+}
+
+export default Header;
+
