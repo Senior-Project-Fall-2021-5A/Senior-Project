d0d2cf21c599359fd4c2f3239bd4d5c948a7b499
frostbyte000jm <james@getjames.com>
2021-09-10 01:10:34 -0400

    Button is now only on Home

--- telemedicine-app/src/App.js
+++ telemedicine-app/src/App.js
@@ -1,14 +1,11 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import Button from 'react-bootstrap/Button'
 import Header from './components/header';
 import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
 import HomePage from "./pages/homepage";
 import About from "./pages/about";
-// import { Button } from 'bootstrap';
-// import { Router, Route, Switch } from "react-router";
-// import {Link} from 'react-router-dom'
+
 
 
 
@@ -17,15 +14,12 @@ function App() {
     <Router>
       <div className="App">
         <Header/>
-        <Link to="/about">
-        <Button variant="primary">
-          About
-        </Button>{' '}
-        </Link>
+        
+        
+        
         <Switch>
           <Route path="/about" component={About} /> 
-          <Route path="/" component={HomePage} />
-          
+          <Route path="/" component={HomePage} />          
         </Switch>
         
 
