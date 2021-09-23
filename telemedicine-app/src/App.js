9ee70dcfe64e5d24b63fb9c15cfc8d70d0a2ac3e
frostbyte000jm <james@getjames.com>
2021-09-22 22:42:17 -0400

    Added Reports Page
    This isn't linked up to any DB, but this is the layout I am going with.

--- telemedicine-app/src/App.js
+++ telemedicine-app/src/App.js
@@ -6,6 +6,9 @@ import {Route, BrowserRouter as Router, Switch, Link} from "react-router-dom";
 import Login from "./pages/login";
 import About from "./pages/about";
 import Homepage from './pages/homepage';
+import Reports from './pages/reports';
+import ReportDisplay from './pages/reportDisplay';
+
 import { Fragment } from 'react';
 
 
@@ -20,6 +23,8 @@ function App() {
         <Switch>
           <Route path="/about" component={About} />
           <Route path='/homepage' component={Homepage}/>
+          <Route path='/reports' component={Reports}/>
+          <Route path='/reportDisplay' component={ReportDisplay}/>
           <Route path="/" exact component={Login} />          
         </Switch>
 
