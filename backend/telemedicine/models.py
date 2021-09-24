5ec10d1b628da220179c7bd2bb61329a43e43a79
Pdoro11 <43185183+Pdoro11@users.noreply.github.com>
2021-09-23 20:42:35 -0400

    Init Model development

--- backend/telemedicine/models.py
+++ backend/telemedicine/models.py
@@ -1,3 +1,27 @@
+from django.core.files.storage import FileSystemStorage
 from django.db import models
 
-# Create your models here.
+documentStorage = FileSystemStorage('../storage/documents')
+
+# Model for appointments objects TODO
+class appointments(models.Model):
+    date = models.DateField()
+    time = models.TimeField()
+    location = models.TextField() 
+    details = models.TextField()
+    documents = models.FileField(storage=documentStorage)
+
+# Model for reports objects TODO
+class reports(models.Model):
+    results = models.TextField()
+    documents = models.FileField(storage=documentStorage)
+
+
+# Model for inbox TODO ask about what needs to be shown
+
+
+
+# Model for account information TODO ask what needs to be shown
+
+
+# Model for authentication TODO look into different auth/encrypt methods in Django Docs 
