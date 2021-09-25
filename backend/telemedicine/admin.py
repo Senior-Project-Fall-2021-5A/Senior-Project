da1f93c43317f4b3752980afed68259f68c49cc9
khanhng388 <58678256+khanhng388@users.noreply.github.com>
2021-09-25 16:14:10 -0400

    add account information model

--- backend/telemedicine/admin.py
+++ backend/telemedicine/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from .models import account, appointments, reports
 
+class appointmentsAdmin(admin.ModelAdmin):
+    list_display = ('date','time','location','details','documents')
+
+class reportsAdmin(admin.ModelAdmin):
+    list_display =('results','documents')
+
+class accountAdmin(admin.ModelAdmin):
+    list_display=('firstname','lastname','DoB','address','primarydoctor','primarylocation','documents')
+
+
+admin.site.register(appointments, appointmentsAdmin)
+
+admin.site.register(reports, reportsAdmin)
+
+admin.site.register(account, accountAdmin)
 # Register your models here.
+# test
