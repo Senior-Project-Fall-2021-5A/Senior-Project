081540bae708b865032860d73bafcf5adbf36c60
Jorge <jorgecab796@gmail.com>
2021-09-25 23:37:06 -0400

    Inbox start

---
+++ telemedicine-app/src/pages/inbox.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import './reports.css';
+
+import {AgGridReact} from 'ag-grid-react';
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-balham.css'
+import { Link } from 'react-router-dom';
+import Canvas from '../components/Canvas';
+import '../components/Canvas.css';
+import Footer from '../components/Footer';
+import Navbar from '../components/Navbar/Navbar';
+
+class Inbox extends React.Component{
+    
+    }
+
+
+
+
+export default Inbox;
