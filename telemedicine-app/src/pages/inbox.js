b15f88131ea842367240ef7afceccca7508c21c8
Jorge <jorgecab796@gmail.com>
2021-09-28 17:41:00 -0400

    Update inbox.js

--- telemedicine-app/src/pages/inbox.js
+++ telemedicine-app/src/pages/inbox.js
@@ -1,18 +1,40 @@
 import React from 'react'
-import './reports.css';
 
+//Not used yet
 import {AgGridReact} from 'ag-grid-react';
-import 'ag-grid-community/dist/styles/ag-grid.css'
-import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 import { Link } from 'react-router-dom';
 import Canvas from '../components/Canvas';
+
+import 'ag-grid-community/dist/styles/ag-grid.css'
+import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 import '../components/Canvas.css';
 import Footer from '../components/Footer';
 import Navbar from '../components/Navbar/Navbar';
 
 class Inbox extends React.Component{
-    
+    render() {
+        return (
+            
+            <div class="title"> 
+                <Navbar/>
+                <div className='page-canvas-outer'>
+                    <div className='page-canvas-inner'>
+                        <div className='canvas'>
+                            <h1>Inbox</h1>
+                            <div class="wrapper">                                
+                                
+                            </div>
+                        </div>
+                    </div>                    
+                </div>
+                <Footer/>
+            </div>
+            
+            
+            
+        )
     }
+}
 
 
 
