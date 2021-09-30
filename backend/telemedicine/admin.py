eeadfaeb28447eeeb70227fdb4d7ec66f79a2804
Pdoro11 <43185183+Pdoro11@users.noreply.github.com>
2021-09-30 17:42:46 -0400

    Added in 'doctors_notes' and 'reports' field to Appointments collection model

--- backend/telemedicine/admin.py
+++ backend/telemedicine/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import account, appointments, reports
 
 class appointmentsAdmin(admin.ModelAdmin):
-    list_display = ('patient_name','date','time','location','details','documents')
+    list_display = ('patient_name', 'doctor_name', 'date','time','location','doctors_notes','reports')
 
 class reportsAdmin(admin.ModelAdmin):
     list_display = ('results','documents')
