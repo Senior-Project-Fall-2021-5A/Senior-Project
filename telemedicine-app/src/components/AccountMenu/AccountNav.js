6f6266a69e15a812e1ecde9edea61300347c8f63
swalla50 <46326914+swalla50@users.noreply.github.com>
2021-09-30 14:52:41 -0400

    Update to the account page
    -created a navigation page
    -Fixed the registration form location

--- telemedicine-app/src/components/AccountMenu/AccountNav.js
+++ telemedicine-app/src/components/AccountMenu/AccountNav.js
@@ -1,42 +1,35 @@
 import React from 'react'
+import './AccountNav.css'
+import {Tab, Row, Col, Nav} from 'react-bootstrap'
 
-const AccountMenuItems= [
-    {
-        title: 'General Information',
-        url: './generalinformation',
-        cName:'account-nav-item'
-    },
-    {
-        title: 'Reports',
-        url: '/reports',
-        cName:'account-nav-item'
-    },
-    {
-        title: 'Inbox',
-        url: '/inbox',
-        cName:'account-nav-item'
-    },
-    {
-        title: 'Sign Up',
-        url: '#',
-        cName:'account-nav-item'
-    }
-]
+
 function AccountNav() {
     return (
-        <div className='account-vertical-nav'>
-            <ul className='account-navmenu' style={{listStyle:'none', textAlign:"left"}}>
-                    {AccountMenuItems.map((item, index) => {
-                        return (
-                            <li classname='nav-list'key={index}>
-                                <a className={item.cName} href={item.url}>
-                                    {item.title}
-                                </a>
-                            </li>
-                        )
-                    })}
-            </ul>
-        </div>
+        <Tab.Container id="left-tabs-example" defaultActiveKey="first">
+  <Row style={{width:'100%', marginLeft:'0px', marginRight:'0px', height:'100%'}}>
+    <Col sm={3}>
+      <Nav variant="pills" style={{width:'100%'}} className="flex-column">
+        <Nav.Item>
+          <Nav.Link eventKey="first">General Information</Nav.Link>
+        </Nav.Item>
+        
+        <Nav.Item>
+          <Nav.Link href='/'>Logout</Nav.Link>
+        </Nav.Item>
+      </Nav>
+    </Col>
+    <Col sm={9}>
+      <Tab.Content>
+        <Tab.Pane eventKey="first">
+          This is where the user will be able to view their name, user name, current password, doctor, and change attributes of their profile.
+        </Tab.Pane>
+        <Tab.Pane eventKey="second">
+          Hola Steven
+        </Tab.Pane>
+      </Tab.Content>
+    </Col>
+  </Row>
+</Tab.Container>
     )
 }
 
--- telemedicine-app/src/pages/MyAccount.js
+++ telemedicine-app/src/pages/MyAccount.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Accountnav from '../components/AccountMenu/AccountNav'
 import Navbar from '../components/Navbar/Navbar'
+import './MyAccount.css'
 
 
 function MyAccount() {
@@ -11,7 +12,9 @@ function MyAccount() {
                     <div className='canvas-inner'>
                         <div className='page-canvas'>
                             <div className='Dashboard-row'>
-                                <Accountnav/>
+                                <div className='account-panel'>
+                                    <Accountnav/>
+                                </div>
                             </div>
                         </div>
                     </div>
