2d2920398fa404c3dcfe59f5665442fea85e89fb
Peter Doro <peterdoro@outlook.com>
2021-09-29 23:00:32 -0400

    Added in page routing to dropdown items

--- telemedicine-app/src/components/Navbar/Profile.js
+++ telemedicine-app/src/components/Navbar/Profile.js
@@ -8,10 +8,10 @@ function Profile() {
             <DropdownButton  className='profile-button' title={<span className='profile-icon'><i class="fas fa-user-circle fa-2x"></i></span>} >
                 <span className="visually-hidden">unread messages</span>
                 <Dropdown.Item eventKey="1">My Account</Dropdown.Item>
-                <Dropdown.Item eventKey="2">My Appointments</Dropdown.Item>
-                <Dropdown.Item eventKey="3">My Reports</Dropdown.Item>
-                <Dropdown.Item eventKey="4">My Inbox</Dropdown.Item>
-                <Dropdown.Item eventKey="5" href= '/'>Logout</Dropdown.Item>
+                <Dropdown.Item eventKey="2" href='/appointments'>My Appointments</Dropdown.Item>
+                <Dropdown.Item eventKey="3" href='/reports'>My Reports</Dropdown.Item>
+                <Dropdown.Item eventKey="4" href ='/inbox'>My Inbox</Dropdown.Item>
+                <Dropdown.Item eventKey="5" href='/'>Logout</Dropdown.Item>
             </DropdownButton>
             <DropdownButton className="notification-button" title={<span className='notification-icon'><Badge bg="danger">9</Badge><i class="fas fa-inbox"></i></span>}>
                 <span className="visually-hidden">unread messages</span>
