7279950811569b498c3c3283e02f0243c30385e6
Jorge <jorgecab796@gmail.com>
2021-09-30 23:41:50 -0400

    Inbox update

--- telemedicine-app/src/pages/inbox.js
+++ telemedicine-app/src/pages/inbox.js
@@ -1,38 +1,54 @@
 import React from 'react'
+import "./inbox.css";
 
 //Not used yet
-import {AgGridReact} from 'ag-grid-react';
+import { AgGridReact } from 'ag-grid-react';
 import { Link } from 'react-router-dom';
 import Canvas from '../components/Canvas';
+
+import Tabs from '@restart/ui/esm/Tabs';
+import Tab from 'react-bootstrap/Tab';
+import Navbar from '../components/Navbar/Navbar';
+import { Card, Button, Nav } from 'react-bootstrap'
+import { Accordion } from 'react-bootstrap';
+import Message from '../components/inbox components/message';
 
 import 'ag-grid-community/dist/styles/ag-grid.css'
 import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 import '../components/Canvas.css';
 import Footer from '../components/Footer';
-import Navbar from '../components/Navbar/Navbar';
 
-class Inbox extends React.Component{
+
+
+class Inbox extends React.Component {
     render() {
         return (
-            
-            <div class="title"> 
-                <Navbar/>
+
+            <div class="title">
+                <Navbar />
                 <div className='page-canvas-outer'>
                     <div className='page-canvas-inner'>
                         <div className='canvas'>
-                            <h1>Inbox (A work in progress)</h1>
-                            <div class="wrapper">                                
-                                
+                            <div class="wrapper">
+                                <div className="emailList">
+                                    <div className="top">
+                                        <h1 className="title">Inbox</h1>
+                                        <Button classname="button" variant="primary">Compose</Button>
+                                        <Message />
+                                    </div>
+                                    
+
+                                </div>
                             </div>
                         </div>
-                    </div>                    
+                    </div>
                 </div>
-                <Footer/>
+                <Footer />
             </div>
             // comment for pushing
 
-            
-            
+
+
         )
     }
 }
