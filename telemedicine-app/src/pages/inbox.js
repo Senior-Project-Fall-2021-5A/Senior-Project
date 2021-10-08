419f3f4b27cb041840096e5c5c2078a4e9f60642
Jorge <jorgecab796@gmail.com>
2021-10-07 22:58:15 -0400

    inbox update
    Accordion made.
    
    Some issues with CSS still and scrolling

--- telemedicine-app/src/pages/inbox.js
+++ telemedicine-app/src/pages/inbox.js
@@ -1,23 +1,22 @@
 import React from 'react'
 import "./inbox.css";
 
-//Not used yet
 import { AgGridReact } from 'ag-grid-react';
 import { Link } from 'react-router-dom';
 import Canvas from '../components/Canvas';
-
 import Tabs from '@restart/ui/esm/Tabs';
 import Tab from 'react-bootstrap/Tab';
-import Navbar from '../components/Navbar/Navbar';
 import { Card, Button, Nav } from 'react-bootstrap'
 import { Accordion } from 'react-bootstrap';
 import Message from '../components/inbox components/message';
 
+import Navbar from '../components/Navbar/Navbar';
 import 'ag-grid-community/dist/styles/ag-grid.css'
 import 'ag-grid-community/dist/styles/ag-theme-balham.css'
 import '../components/Canvas.css';
 import Footer from '../components/Footer';
 import Top from "../components/inbox components/Top"
+import AccordionCustom from '../components/inbox components/AccordionCustom';
 
 
 
@@ -33,30 +32,9 @@ class Inbox extends React.Component {
                         <div className='canvas'>
 
                             <Top />
+                            <AccordionCustom /> 
                             
-                            {/* <div className="accordion">
-                                    {data.map((item, i) => (
-                                        <div className="item">
-                                            <div className="title">
-                                                <h2>{item.message}</h2>
-                                            </div>
-                                            <div className="content">
-                                                <p>{item.content}</p>
-                                            </div>
-                                        </div>
-                                    ))}
-                                </div> */}
-
-
-                            {/* <div className="emailList">
-                                    <div className="top">
-                                        <h1 className="title">Inbox</h1>
-                                        <Button classname="button" variant="primary">Compose</Button>
-                                        <Message />
-                                    </div>
-                                    
-
-                                </div> */}
+                            {/* <Accordion /> */}
 
                         </div>
                     </div>
