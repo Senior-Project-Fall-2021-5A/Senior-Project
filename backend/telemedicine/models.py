f1f46b5c0ad996bfe8bcf75cfee1964f20755e16
khanhng388 <58678256+khanhng388@users.noreply.github.com>
2021-10-13 21:32:22 -0400

    add new model and update

--- backend/telemedicine/models.py
+++ backend/telemedicine/models.py
@@ -1,9 +1,12 @@
+from os import link, name, times
+from django.core.checks import messages
 from django.core.files.storage import FileSystemStorage
 from django.db import models
 
 documentStorage = FileSystemStorage('../storage/documents')
-
+"""
 # Model for appointments objects TODO
+
 class appointments(models.Model):
     patient_name = models.CharField(
         help_text="Patients full name",
@@ -45,5 +48,154 @@ class account(models.Model):
 
     def _str_(self):
         return self.DoB
+"""
+# Model for user TODO ask what needs to be shown
+class user(models.Model):
+    userGUID = models.UUIDField()
+    type = models.CharField() # look into UUID field or NUllBoolean field may find a easy way
+    userName = models.TextField()
+    password = models.TextField()
+
+    def __str__(self):
+        return self.userName
+
+# Model for doctor TODO ask what needs to be shown
+class doctorProfile(models.Model):
+    userGUID = models.UUIDField()
+    doctorProfileUID = models.UUIDField()
+    first_name = models.CharField()
+    mid_name = models.CharField()
+    last_name = models.CharField()
+    DoB = models. DateField()
+    address1 = models.TextField()
+    address2 = models.TextField()
+    city = models.CharField()
+    state = models.CharField()
+    zip = models.CharField()
+    idnum = models.TextField()
+    email = models.EmailField()
+    phone1 = models.CharField()
+    phone2 = models.CharField()
+    phone3 = models.CharField()
+    documents = models.FileField(storage=documentStorage)
+    def __str__(self):
+        return self.doctorUID
+
+class paitientProfile (models.Model):
+    userGUID = models.UUIDField()
+    paitientProfileUID = models.UUIDField()
+    first_name = models.CharField()
+    mid_name = models.CharField()
+    last_name = models.CharField()
+    DoB = models. DateField()
+    address1 = models.TextField()
+    address2 = models.TextField()
+    city = models.CharField()
+    state = models.CharField()
+    zip = models.CharField()
+    idnum = models.TextField()
+    email = models.EmailField()
+    phone1 = models.CharField()
+    phone2 = models.CharField()
+    phone3 = models.CharField()
+    Insurance01UID = models.AutoField()
+    Insurance02UID = models.AutoField()
+    Insurance03UID = models.AutoField()
+    documents = models.FileField(storage=documentStorage)
+    def _str_(self):
+        return self.paitientUID
+# Model for demograhpic TODO ask what needs to be shown
+
+
+# Model for appointment TODO ask what needs to be shown
+class appointments(models.Model):
+    appointmentUID = models.UUIDField()
+    patientProfileUID = models.UUIDField()
+    doctorProfileUID = models.UUIDField
+    date = models.DateField()
+    time = models.TimeField()
+    locationUID = models.UUIDField() 
+    doctors_notes = models.TextField()
+    reports = models.FileField(storage=documentStorage)
+
+    def _str_(self):
+        return self.appointmentUID
+
+
+# Model for inbox TODO ask what needs to be shown
+class inbox(models.Model):
+    inboxUID = models.UUIDField()
+    userGuid = models.UUIDField()
+    toDemoUID = models.UUIDField()
+    subject = models.CharField(
+        help_text="subject", max_length=200)
+    boby = models.TextField()
+    date = models.DateField()
+    time = models.TimeField()
+    documents = models.FileField(storage=documentStorage)
+    isRead = models.BooleanField()
+
+    def __str__(self):
+        return self.inboxUID
+
+# Model for reports TODO ask what needs to be shown
+class reports(models.Model):
+    reportUID = models.UUIDField
+    appointmentUID = models.UUIDField()
+    date = models.DateField()
+    time = models.TimeField()
+    details = models.TextField()
+    documents = models.FileField(storage=documentStorage)
+
+    def _str_(self):
+        return self.details
+
+# Model for insurance TODO ask what needs to be shown
+class insurance(models.Model):
+    insuranceUID = models.UUIDField()
+    name = models.CharField(
+        help_text="address",max_length=200)
+    accountid = models.CharField(
+        help_text="address",max_length=200)
+    insuranceType = models.IntegerField()
+
+    def __str__(self):
+        return self.insuranceUID
+
+# Model for location TODO ask what needs to be shown
+class location(models.Model):
+    locationUID = models.UUIDField()
+    name = models.TextField(
+        help_text="name place",max_length=200)
+    address1 = models.TextField(
+        help_text="address",max_length=200)
+    address2 = models.TextField(
+        help_text="address",max_length=200)
+    city = models.CharField(
+        help_text="address",max_length=200)
+    State = models.CharField(
+        help_text="address",max_length=200)
+    zip = models.CharField(
+        help_text="address",max_length=200)
+
+    def __str__(self):
+        return self.locationUID
+
+# Model for notifications TODO ask what needs to be shown
+class notification(models.Model):
+    notificationUID = models.UUIDField()
+    appointmentUID = models.UUIDField()
+    reportUID = models.UUIDField()
+    inboxUID = models.UUIDField()
+    messages = models.CharField(
+        help_text="reminder", max_length=200)
+    link = models.URLField()
+    date = models.DateField()
+    time = models.TimeField()
+    doEmail = models.BooleanField()
+
+    def __str__(self):
+        return self.notificationUID
+
 
 # Model for authentication TODO look into different auth/encrypt methods in Django Docs 
