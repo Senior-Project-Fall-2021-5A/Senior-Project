bdc1c10bbc41ec4085f93331a52a741f79abea20
khanhng388 <58678256+khanhng388@users.noreply.github.com>
2021-10-19 17:59:56 -0400

    fix error the admin file can not recognize the variable in mdole.py

--- backend/telemedicine/admin.py
+++ backend/telemedicine/admin.py
@@ -20,34 +20,48 @@ admin.site.register(account, accountAdmin)
 
 """
 
+
 class userAdmin(admin.ModelAdmin):
     list_display = ('userGUID', 'type', 'userName', 'password')
 
+
 class doctorProfileAdmin(admin.ModelAdmin):
-    list_display = ('userGUID', 'doctorProfileUID', 'first_name', 'last_name', 'DoB', 'address1', 'address2', 
+    list_display = ('userGUID', 'doctorProfileUID', 'first_name', 'last_name', 'DoB', 'address1', 'address2',
                     'city', 'state', 'zip', 'idnum', 'email', 'phone1', 'phone2', 'phone3', 'documents')
 
+
 class paitientProfileAdmin(admin.ModelAdmin):
-    list_display = ('userGUID', 'paitientProfileUID', 'first_name', 'last_name', 'DoB', 'address1', 'address2', 
-                    'city', 'state', 'zip', 'idnum', 'email', 'phone1', 'phone2', 'phone3','Insurance01UID', 'Insurance02UID', 'Insurance03UID', 'documents')
+    list_display = ('userGUID', 'paitientProfileUID', 'first_name', 'last_name', 'DoB', 'address1', 'address2',
+                    'city', 'state', 'zip', 'idnum', 'email', 'phone1', 'phone2', 'phone3', 'Insurance01UID', 'Insurance02UID', 'Insurance03UID', 'documents')
+
 
 class appointmentsAdmin(admin.ModelAdmin):
-    list_display = ('appointmentUID', 'paitientProfileUID', 'doctorProfileUID', 'date', 'time', 'locationUID', 'doctors_notes', 'reports')
+    list_display = ('appointmentUID', 'paitientProfileUID', 'doctorProfileUID',
+                    'date', 'time', 'locationUID', 'doctors_notes', 'reports')
+
 
 class inboxAdmin(admin.ModelAdmin):
-    list_display = ('inboxUID', 'userGUID', 'toDemoUID', 'subject', 'body', 'date', 'time', 'documents', 'isRead')
+    list_display = ('inboxUID', 'userGUID', 'toDemoUID', 'subject',
+                    'body', 'date', 'time', 'documents', 'isRead')
+
 
 class reportsAdmin(admin.ModelAdmin):
-    list_display = ('reportUID', 'appointmentUID', 'date', 'time', 'details', 'documents')
+    list_display = ('reportUID', 'appointmentUID', 'date',
+                    'time', 'details', 'documents')
+
 
 class insuranceAdmin(admin.ModelAdmin):
     list_display = ('insuranceUID', 'name', 'accountid', 'insuranceType')
 
+
 class locationAdmin(admin.ModelAdmin):
-    list_display = ('locationUID', 'name', 'address1', 'address2', 'city', 'State', 'zip')
+    list_display = ('locationUID', 'name', 'address1',
+                    'address2', 'city', 'State', 'zip')
+
 
 class notificationAdmin(admin.ModelAdmin):
-    list_display = ('notificationUID', 'appointmentUID', 'reportUID', 'inboxUID', 'messages', 'link', 'date', 'time', 'doEmail')
+    list_display = ('notificationUID', 'appointmentUID', 'reportUID',
+                    'inboxUID', 'messages', 'link', 'date', 'time', 'doEmail')
 
 
 admin.site.register(user, userAdmin)
@@ -60,4 +74,4 @@ admin.site.register(insurance, insuranceAdmin)
 admin.site.register(location, locationAdmin)
 admin.site.register(notification, notificationAdmin)
 
-# test 
+# test
