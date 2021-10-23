b2eacc8f5d4aa01c9d186e2538ec2b1d79edc32d
frostbyte000jm <james@getjames.com>
2021-10-23 13:22:50 -0400

    simple changes

--- telemedicine-app/src/pages/reports_page/reports.js
+++ telemedicine-app/src/pages/reports_page/reports.js
@@ -29,6 +29,21 @@ function Reports() {  //const App = () =>
         {guid: 1, report: 'Test tester test', physician: 'Dr Who', date: '09/15/2021'},
         {guid: 2, report: 'Time Machine', physician: 'Dr Brown', date: '09/05/2021'},
         {guid: 3, report: 'McTesty Test', physician: 'Dr House', date: '09/11/2021'},
+        {guid: 1, report: 'Test tester test', physician: 'Dr Who', date: '09/15/2021'},
+        {guid: 2, report: 'Time Machine', physician: 'Dr Brown', date: '09/05/2021'},
+        {guid: 3, report: 'McTesty Test', physician: 'Dr House', date: '09/11/2021'},
+        {guid: 1, report: 'Test tester test', physician: 'Dr Who', date: '09/15/2021'},
+        {guid: 2, report: 'Time Machine', physician: 'Dr Brown', date: '09/05/2021'},
+        {guid: 3, report: 'McTesty Test', physician: 'Dr House', date: '09/11/2021'},
+        {guid: 1, report: 'Test tester test', physician: 'Dr Who', date: '09/15/2021'},
+        {guid: 2, report: 'Time Machine', physician: 'Dr Brown', date: '09/05/2021'},
+        {guid: 3, report: 'McTesty Test', physician: 'Dr House', date: '09/11/2021'},
+        {guid: 1, report: 'Test tester test', physician: 'Dr Who', date: '09/15/2021'},
+        {guid: 2, report: 'Time Machine', physician: 'Dr Brown', date: '09/05/2021'},
+        {guid: 3, report: 'McTesty Test', physician: 'Dr House', date: '09/11/2021'},
+        {guid: 1, report: 'Test tester test', physician: 'Dr Who', date: '09/15/2021'},
+        {guid: 2, report: 'Time Machine', physician: 'Dr Brown', date: '09/05/2021'},
+        {guid: 3, report: 'McTesty Test', physician: 'Dr House', date: '09/11/2021'},
    
         
 
