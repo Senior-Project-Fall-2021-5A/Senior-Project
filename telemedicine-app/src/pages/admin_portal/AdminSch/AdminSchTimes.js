a5e2abc6f1d7ff9b5220eedeb7964d1da15e3eaf
frostbyte000jm <james@getjames.com>
2021-10-24 16:46:50 -0400

    Update AdminSchTimes

--- telemedicine-app/src/pages/admin_portal/AdminSch/AdminSchTimes.js
+++ telemedicine-app/src/pages/admin_portal/AdminSch/AdminSchTimes.js
@@ -17,24 +17,39 @@ const styleTDBox = {
 
 const AdminSchTimes = ({}) => {
     const [checkedTime01, setCheckedTime01] = React.useState(false);
+    const [checkedTime02, setCheckedTime02] = React.useState(false);
+    const [checkedTime03, setCheckedTime03] = React.useState(false);
+    const [checkedTime04, setCheckedTime04] = React.useState(false);
+    const [checkedTime05, setCheckedTime05] = React.useState(false);
+    const [checkedTime06, setCheckedTime06] = React.useState(false);
+    const [checkedTime07, setCheckedTime07] = React.useState(false);
+    const [checkedTime08, setCheckedTime08] = React.useState(false);
+    const [checkedTime09, setCheckedTime09] = React.useState(false);
+    const [checkedTime10, setCheckedTime10] = React.useState(false);
+    const [checkedTime11, setCheckedTime11] = React.useState(false);
+    const [checkedTime12, setCheckedTime12] = React.useState(false);
+    const [checkedTime13, setCheckedTime13] = React.useState(false);
+    const [checkedTime14, setCheckedTime14] = React.useState(false);
+    const [checkedTime15, setCheckedTime15] = React.useState(false);
+    const [checkedTime16, setCheckedTime16] = React.useState(false);
 
     const data = [
         {name: "time01", value: {checkedTime01}, text: "09:00 am"},
-        {name: "time02", value: {checkedTime01}, text: "09:30 am"},
-        {name: "time03", value: {checkedTime01}, text: "10:00 am"},
-        {name: "time04", value: {checkedTime01}, text: "10:30 am"},
-        {name: "time05", value: {checkedTime01}, text: "11:00 am"},
-        {name: "time06", value: {checkedTime01}, text: "11:30 am"},
-        {name: "time07", value: {checkedTime01}, text: "12:00 am"},
-        {name: "time08", value: {checkedTime01}, text: "12:30 am"},
-        {name: "time09", value: {checkedTime01}, text: "01:00 am"},
-        {name: "time10", value: {checkedTime01}, text: "01:30 am"},
-        {name: "time11", value: {checkedTime01}, text: "02:00 am"},
-        {name: "time12", value: {checkedTime01}, text: "02:30 am"},
-        {name: "time13", value: {checkedTime01}, text: "03:00 am"},
-        {name: "time14", value: {checkedTime01}, text: "03:30 am"},
-        {name: "time15", value: {checkedTime01}, text: "04:00 am"},
-        {name: "time16", value: {checkedTime01}, text: "04:30 am"},
+        {name: "time02", value: {checkedTime02}, text: "09:30 am"},
+        {name: "time03", value: {checkedTime03}, text: "10:00 am"},
+        {name: "time04", value: {checkedTime04}, text: "10:30 am"},
+        {name: "time05", value: {checkedTime05}, text: "11:00 am"},
+        {name: "time06", value: {checkedTime06}, text: "11:30 am"},
+        {name: "time07", value: {checkedTime07}, text: "12:00 am"},
+        {name: "time08", value: {checkedTime08}, text: "12:30 am"},
+        {name: "time09", value: {checkedTime09}, text: "01:00 am"},
+        {name: "time10", value: {checkedTime10}, text: "01:30 am"},
+        {name: "time11", value: {checkedTime11}, text: "02:00 am"},
+        {name: "time12", value: {checkedTime12}, text: "02:30 am"},
+        {name: "time13", value: {checkedTime13}, text: "03:00 am"},
+        {name: "time14", value: {checkedTime14}, text: "03:30 am"},
+        {name: "time15", value: {checkedTime15}, text: "04:00 am"},
+        {name: "time16", value: {checkedTime16}, text: "04:30 am"},
         
     ]
 
