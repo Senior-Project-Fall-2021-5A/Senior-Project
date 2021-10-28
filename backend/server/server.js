d17ead07abfbfb02d0b2ec2892b31b01877984a9
Pdoro11 <43185183+Pdoro11@users.noreply.github.com>
2021-10-28 03:48:20 -0400

    Added node engine version and re-added file upload

--- backend/server/server.js
+++ backend/server/server.js
@@ -34,7 +34,35 @@ app.post('/register', async (req, res) => {
     res.send('User Added!')
 })
 
+/// upload file
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads')
+    },
+    filename: (req, file, cb) => {
+        const ext = path.extname(file.originalname);
+        const filePath = `file/${id}${ext}`;
+        file.create({ filePath: filePath })
+            .then(() => {
+                cb(null, filePath)
+            });
+    }
+})
 
+const upload = multer({ storage }); // or simply { dest: 'uploads/' }
+app.use(express.static('public'));
+app.use(express.static('uploads'));
+
+app.post('/upload', upload.array('file'), (req, res) => {
+    return res.redirect('/');
+});
+
+app.get('/file', (req, res) => {
+    file.find()
+        .then((files) => {
+            return res.json({ status: 'OK', files});
+        })
+});
 app.listen(PORT, () => { console.log('Connection SUCCESSFUL') });
 
 module.exports = app;
