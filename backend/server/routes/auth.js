314e740e109f059c2deeb8a78a1612c9cc6fb839
Pdoro11 <43185183+Pdoro11@users.noreply.github.com>
2021-10-28 13:50:28 -0400

    JWT key env

--- backend/server/routes/auth.js
+++ backend/server/routes/auth.js
@@ -2,7 +2,6 @@ const express = require('express')
 const router = express.Router();
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
-const config = require('config');
 const { check, validationResult } = require('express-validator');
 const User = require('../models/User');
 
@@ -43,7 +42,7 @@ router.post(
 
       jwt.sign(
         payload,
-        config.get('jwtKey'),
+        process.env.JWT_SECRET,
         { expiresIn: 360000 },
         (err, token) => {
           if (err) throw err;
