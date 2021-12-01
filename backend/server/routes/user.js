aa5ddaf0841834961bddbd319c443e0876e8a0c6
Pdoro11 <43185183+Pdoro11@users.noreply.github.com>
2021-12-01 16:07:12 -0500

    Add role for user register route

--- backend/server/routes/user.js
+++ backend/server/routes/user.js
@@ -39,7 +39,7 @@ router.post(
       return res.status(400).json({ errors: errors.array() });
     }
 
-    const { name, email, password } = req.body;
+    const { name, email, password, role } = req.body;
     try {
       let user = await User.findOne({ email });
 
@@ -52,7 +52,8 @@ router.post(
       user = new User({
         name,
         email,
-        password
+        password,
+        role
       });
 
       const salt = await bcrypt.genSalt(10);
