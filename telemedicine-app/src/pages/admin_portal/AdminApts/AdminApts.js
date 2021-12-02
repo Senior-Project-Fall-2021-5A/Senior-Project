60623f31567a5a68ef1189d898e7300f279fe224
frostbyte000jm <james@getjames.com>
2021-12-02 02:06:42 -0500

    Date box

--- telemedicine-app/src/pages/admin_portal/AdminApts/AdminApts.js
+++ telemedicine-app/src/pages/admin_portal/AdminApts/AdminApts.js
@@ -3,13 +3,16 @@ import './adminApts.css'
 import ObjLink from '../../../components/Objects/ObjLink'
 import AdminAptsTable from './AdminAptsTable'
 import PopUpApptSelect from '../PopupPages/PopUpApptSelect'
-
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
 
 
 const AdminApts = ({  }) => {
 
     //New Patient Popup handler
     const [allApptsPopup, setAllApptsPopup] = React.useState(false);    
+    const [date,setDate] = React.useState(new Date());
+
     const allApptsClick = ( event ) => {
         console.log("New Patient Click");
         console.log("click", event);
@@ -37,6 +40,26 @@ const AdminApts = ({  }) => {
                         doLink = "false"   
                     />                                    
                                     
+                </div>
+                <div className="popup_spread_grid"
+                    style={{
+                        display: "flex",
+                        flexWrap: "wrap",
+                       
+                        justifyContent: "center",
+                        position: 'relative', 
+                        top: '-30px',
+                    }}
+                >
+                    {/* Date Select */}
+                    <div>
+                        <DatePicker                            
+                            selected={date}
+                            onChange={e=>setDate(e)}
+                        />
+                    </div>
+
+                    
                 </div>
             </div>
 
