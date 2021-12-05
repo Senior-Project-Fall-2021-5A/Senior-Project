32770fa55848c3cafa051bdc581c572e9af5e642
Jorge <43802790+jorgecab796@users.noreply.github.com>
2021-12-05 06:31:41 -0500

    Update PopUpComposeMessage.js

--- telemedicine-app/src/pages/admin_portal/PopupPages/PopUpComposeMessage.js
+++ telemedicine-app/src/pages/admin_portal/PopupPages/PopUpComposeMessage.js
@@ -4,21 +4,21 @@ import "react-datepicker/dist/react-datepicker.css";
 import PopUpWindow from '../../../components/Objects/ObjPopUpWindow'
 import ObjButton from '../../../components/Objects/ObjButton'
 
-const PopUpComposeMessage = ( {trigger,setTrigger} ) => {
+const PopUpComposeMessage = ({ trigger, setTrigger }) => {
     //declarations
-    const [txtPatientFName,setPatientFName] = React.useState("");
-    const [txtPatientMName,setPatientMName] = React.useState("");
-    const [txtPatientLName,setPatientLName] = React.useState("");
-    const [txtPatientEmail,setPatientEmail] = React.useState("");
-    const [txtPatientPass,setPatientPass] = React.useState("");
-    const [textDoctorID,setDoctorID] = React.useState("");
-    const [boolError,setBoolError] = React.useState(false);
-    const [txtError,setError] = React.useState("");
+    const [txtPatientFName, setPatientFName] = React.useState("");
+    const [txtPatientMName, setPatientMName] = React.useState("");
+    const [txtPatientLName, setPatientLName] = React.useState("");
+    const [txtPatientEmail, setPatientEmail] = React.useState("");
+    const [txtPatientPass, setPatientPass] = React.useState("");
+    const [textDoctorID, setDoctorID] = React.useState("");
+    const [boolError, setBoolError] = React.useState(false);
+    const [txtError, setError] = React.useState("");
 
     /***************************************************** 
             Temp Needs Link to get actual list of docs
     ******************************************************/
-    const listOfDoctors =[
+    const listOfDoctors = [
         {
             label: "Doctor1",
             value: "00001",
@@ -41,30 +41,30 @@ const PopUpComposeMessage = ( {trigger,setTrigger} ) => {
                     Event Handlers
     ******************************************************/
     //Doctor Select
-    const onDoctorSelect = ( event ) => {
-        console.log("onDoctorSelect - ",event);
+    const onDoctorSelect = (event) => {
+        console.log("onDoctorSelect - ", event);
         console.log("Value set: ", event.target.value);
         setDoctorID(event.target.value);
     }
-    
+
     // Create Patient
-    const onSubmit = ( event ) => {
+    const onSubmit = (event) => {
         console.log(event);
-        console.log("idk yet");   
-        
+        console.log("idk yet");
+
         // This is the message object that will be sent
         const person = {
             firstName: "John",
             lastName: "Doe",
             age: 50,
             eyeColor: "blue"
-          };
-        
+        };
+
         if (textDoctorID == "_placeholder_" || textDoctorID == "" || txtPatientFName == "" || txtPatientMName == "" ||
-         txtPatientLName == "" || txtPatientEmail == "" || txtPatientPass == "") {
+            txtPatientLName == "" || txtPatientEmail == "" || txtPatientPass == "") {
             setBoolError(true);
             setError("Please Fill out all the above Information.");
-        }else{            
+        } else {
             setBoolError(false);
             setDoctorID("");
             setPatientFName("");
@@ -75,14 +75,14 @@ const PopUpComposeMessage = ( {trigger,setTrigger} ) => {
             setTrigger(false);
         }
     }
-    
+
     return (
-    
+
         <PopUpWindow
-                trigger = {trigger}
-                setTrigger = {setTrigger}
-                header = "Compose Message"
-        >    
+            trigger={trigger}
+            setTrigger={setTrigger}
+            header="Compose Message"
+        >
             {/* Grid */}
             <div className="popup_container"
                 style={{
@@ -95,25 +95,25 @@ const PopUpComposeMessage = ( {trigger,setTrigger} ) => {
                 {/* Doctor Select */}
                 <div className="popup_label_grid"
                     style={{
-                        gridRow:1,
-                        gridColumn:1,
+                        gridRow: 1,
+                        gridColumn: 1,
                     }}
                 >
                     <h5 className="popup_label">Doctor:</h5>
                 </div>
                 <div className="popup_inputs_grid"
                     style={{
-                        gridRow:1,
-                        gridColumn:2,
+                        gridRow: 1,
+                        gridColumn: 2,
                     }}
-                >                 
+                >
                     <select
                         style={{
                             height: "25px",
                             width: "300px",
                             textAlign: "left",
-                        }}                        
-                        onChange={e=>onDoctorSelect(e)}  
+                        }}
+                        onChange={e => onDoctorSelect(e)}
                     >
                         <option value="_placeholder_">Select Doctor</option>
                         {listOfDoctors.map((option) => (
@@ -123,25 +123,25 @@ const PopUpComposeMessage = ( {trigger,setTrigger} ) => {
                     </select>
                 </div>
 
-                {/* Message */}
+                {/* Subject */}
                 <div className="popup_label_grid"
                     style={{
-                        gridRow:2,
-                        gridColumn:1,
+                        gridRow: 2,
+                        gridColumn: 1,
                     }}
                 >
-                    <h5 className="popup_label">Message:</h5>
+                    <h5 className="popup_label">Subject:</h5>
                 </div>
                 <div className="popup_inputs_grid"
                     style={{
-                        gridRow:2,
-                        gridColumn:2,
+                        gridRow: 2,
+                        gridColumn: 2,
                     }}
-                >    
+                >
                     <textarea
                         type="text"
                         value={txtPatientMName}
-                        onChange={e=>setPatientMName(e.target.value)}
+                        onChange={e => setPatientMName(e.target.value)}
                         style={{
                             height: "175px",
                             width: "300px",
@@ -149,24 +149,51 @@ const PopUpComposeMessage = ( {trigger,setTrigger} ) => {
                         }}
                     />
                 </div>
-    
-                
+
+                {/* Message */}
+                <div className="popup_label_grid"
+                    style={{
+                        gridRow: 2,
+                        gridColumn: 1,
+                    }}
+                >
+                    <h5 className="popup_label">Message:</h5>
+                </div>
+                <div className="popup_inputs_grid"
+                    style={{
+                        gridRow: 2,
+                        gridColumn: 2,
+                    }}
+                >
+                    <textarea
+                        type="text"
+                        value={txtPatientMName}
+                        onChange={e => setPatientMName(e.target.value)}
+                        style={{
+                            height: "175px",
+                            width: "300px",
+                            textAlign: "left",
+                        }}
+                    />
+                </div>
+
+
                 {/* Button Create */}
                 <div className="popup_spread_grid"
                     style={{
-                        gridRow:7,
-                        gridColumnStart:1,
-                        gridColumnEnd:3,
+                        gridRow: 7,
+                        gridColumnStart: 1,
+                        gridColumnEnd: 3,
                     }}
                 >
-                    <ObjButton                     
-                        
+                    <ObjButton
+
                         text="Send"
-                        onClick={e=>onSubmit(e)}
+                        onClick={e => onSubmit(e)}
                     />
                 </div>
             </div>
-            
+
             {/* Button Error Message */}
             {boolError &&
                 <p
@@ -176,7 +203,7 @@ const PopUpComposeMessage = ( {trigger,setTrigger} ) => {
                 >{txtError}</p>
             }
         </PopUpWindow>
-        
+
     )
 }
 
