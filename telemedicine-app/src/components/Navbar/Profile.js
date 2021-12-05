44dee309b70688d1bea896196f71e152abcfa193
swalla50 <stevenwallacejr@aol.com>
2021-12-05 05:24:50 -0500

    Notification button ready(maybe)

--- telemedicine-app/src/components/Navbar/Profile.js
+++ telemedicine-app/src/components/Navbar/Profile.js
@@ -4,12 +4,33 @@ import Axios from 'axios';
 import './Profile.css';
 import {Badge, Button, DropdownButton, ButtonGroup,Dropdown} from 'react-bootstrap';
 import authUserObject from '../../middleware/authUserObject';
+import './NotificationItem.css'
+import NotificationItem from "./NotificationItem";
+import {notificationData} from './notificaitonData'
 
 function Profile() {
     const [txtGlobalUserID, setGlobalUserID] = useState(authUserObject.userId);
     const [txtGlobalRole, setGlobalRole] = useState(authUserObject.userRole);
     const [boolPortal, setBoolPortal] = useState(false);
 
+
+    let isRead = false;
+    let readCount = notificationData.length;
+    
+    
+    function isClicked(){
+
+            isRead = true;
+            readCount--
+            console.log("isRead is: ", isRead)
+            console.log("This is readCount: ", readCount);
+        if(readCount === 0){
+            document.getElementById("notificationToast").style.display="none";
+        }
+    }
+
+  
+    
     useEffect(() => {
         console.log("Page Open: ",txtGlobalUserID, " Role: ",txtGlobalRole);
         getUserInfo();
@@ -37,12 +58,21 @@ function Profile() {
                 <Dropdown.Item eventKey="5" href= '/'>Logout</Dropdown.Item>
                 {boolPortal && <Dropdown.Item eventKey="6" href= '/adminPortal'>Portal</Dropdown.Item> } 
             </DropdownButton>
-            <DropdownButton className="notification-button" title={<span className='notification-icon'><Badge bg="danger">3</Badge><i class="fas fa-inbox"></i></span>}>
+
+            <DropdownButton  className='notification-btn' title={<span className='profile-icon'><i class="fas fa-inbox fa-2x"></i><Badge id="notificationToast" className = "notification-toast" bg="danger"> </Badge></span>}>
                 <span className="visually-hidden">unread messages</span>
-                <Dropdown.Item eventKey="1">Appointment: Dr. Hayward</Dropdown.Item>
-                <Dropdown.Item eventKey="2">Docort Notes: Dr. Hayward</Dropdown.Item>
-                <Dropdown.Item eventKey="3">Lab Results: Bortolazzo Group</Dropdown.Item>
+                {notificationData.map((data, index) => {
+                    return (
+                        <li classname='nav-list'key={index}>
+                            <div className="notification-item" onClick={isClicked} >
+                                <p className="notificaiton-message">{data.update}</p>
+                                <p className="notification-time">{data.timestamp}</p>
+                            </div>
+                        </li>
+                    )
+                })}
             </DropdownButton>
+
         </ButtonGroup>
 
     )
---
+++ telemedicine-app/src/components/Navbar/notificaitonData.js
@@ -0,0 +1,26 @@
+export const notificationData =[
+    {
+        update: "70 new employees are shifted",
+        timestamp: "2020-05-24 21:27:11"
+    },
+    {
+        update:"80 new employees are shifted",
+        timestamp:"2020-05-24 21:27:12"
+    },
+    {
+        update:"80 new employees are shifted",
+        timestamp:"2020-05-24 21:27:12"
+    },
+    {
+        update:"80 new employees are shifted",
+        timestamp:"2020-05-24 21:27:12"
+    },
+    {
+        update:"80 new employees are shifted",
+        timestamp:"2020-05-24 21:27:12"
+    },
+    {
+        update:"80 new employees are shifted",
+        timestamp:"2020-05-24 21:27:12"
+    }
+];
