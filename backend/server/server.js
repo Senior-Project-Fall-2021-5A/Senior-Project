d6356284a4f40e2c370dd0c1821596e5d24db848
Khanhng3881 <99.khanh.n@gmail.com>
2021-12-05 19:44:04 -0500

    update video call for front-end and back-end code

--- backend/server/server.js
+++ backend/server/server.js
@@ -6,40 +6,53 @@ const app = express();
 const Grid = require("gridfs-stream");
 const mongoose = require("mongoose");
 
-//video call import
-const server = require('http').Server(app);
-const { v4: uuidv4} = require('uuid');
-const io = require('socket.io')(server); 
-const {ExpressPeerServer} = require('peer');
-const peerServer = ExpressPeerServer(server, {
-    debug: true
-});
-app.use(express.static('public'));
-app.use('/peerjs', peerServer);
+//video call
+require('dotenv').config();
+const http = require("http");
+const server = http.createServer(app);
+const socket = require("socket.io");
+const io = socket(server);
 
-app.set('view engine', 'ejs');
+const users = {};
 
- 
-
-
-app.get('/r', (req, res) => {
-    res.redirect(`/${uuidv4()}`);
-});
-
-app.get('/:room', (req, res) => {
-    res.render('room', { roomId: req.param.room })
-});
-
+const socketToRoom = {};
 
 io.on('connection', socket => {
-    socket.on('join-room', (roomId, userId) => {
-        socket.join(roomId);
-        socket.to(roomId).broadcast.emit('user-connected', userId);
-        socket.on('message', message => {
-            io.to(roomId).emit('createMessage', message)
-        })
-    })
-})
+    socket.on("join room", roomID => {
+        if (users[roomID]) {
+            const length = users[roomID].length;
+            if (length === 4) {
+                socket.emit("room full");
+                return;
+            }
+            users[roomID].push(socket.id);
+        } else {
+            users[roomID] = [socket.id];
+        }
+        socketToRoom[socket.id] = roomID;
+        const usersInThisRoom = users[roomID].filter(id => id !== socket.id);
+
+        socket.emit("all users", usersInThisRoom);
+    });
+
+    socket.on("sending signal", payload => {
+        io.to(payload.userToSignal).emit('user joined', { signal: payload.signal, callerID: payload.callerID });
+    });
+
+    socket.on("returning signal", payload => {
+        io.to(payload.callerID).emit('receiving returned signal', { signal: payload.signal, id: socket.id });
+    });
+
+    socket.on('disconnect', () => {
+        const roomID = socketToRoom[socket.id];
+        let room = users[roomID];
+        if (room) {
+            room = room.filter(id => id !== socket.id);
+            users[roomID] = room;
+        }
+    });
+
+});
 
 // Import models for testing
 const UserModel = require('./models/User')
